c5d82c0615f1cfcc04c15145d227bdcfc9898567
ladamsperez <ladamsperezwork@gmail.com>
2022-01-07 15:46:23 -0500

    Remove a hardcoded Django secret key

--- taxes/settings.py
+++ taxes/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0x5!6g$axqn(xw3mbkgpc7sa2gw53ai)6#5y#uj8#%0p9&ngwo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
